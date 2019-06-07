ce6b5eaac9726200e76352f08ad0b0f842677719
github username <github email address>
2019-06-07 16:41:20 +0100

    Done.

---
+++ app/models/coupons_helper.rb
@@ -0,0 +1,7 @@
+class CouponsHelper < ApplicationRecord
+
+   def concat_strings (string1, string2)
+      string1.concat(string2)
+   end
+
+end
---
+++ lib/tasks/secrets.rake
@@ -0,0 +1,5 @@
+namespace :your_namespace do
+  desc 'Description for rake task'
+  task generate_secret_token: :environment do
+  end
+end
